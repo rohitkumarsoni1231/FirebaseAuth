59e1e0575f799d9c06530479a01d06eeaa86e7ee
Rohit Kumar <rohit.kumarsoni1231@gmail.com>
2024-10-15 17:03:42 +0400

    first commit

---
+++ FirebaseAuthentication/FirebaseAuthentication/FirebaseAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthenticationApp.swift
+//  FirebaseAuthentication
+//
+//  Created by Rohit Kumar on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
